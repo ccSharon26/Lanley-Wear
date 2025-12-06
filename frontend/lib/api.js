2746144a53aae35f0cbea9655bd60a6a0d31f8e0
SHARON <99406132+ccSharon26@users.noreply.github.com>
2025-12-06 14:05:59 +0300

    Delete frontend directory

--- frontend/lib/api.js
+++
@@ -1,8 +0,0 @@
-// frontend/lib/api.js
-import { API_BASE_URL } from "./config";
-
-export async function fetchJSON(endpoint, options = {}) {
-  const res = await fetch(`${API_BASE_URL}${endpoint}`, options);
-  if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
-  return res.json();
-}
--- frontend/lib/config.js
+++
@@ -1,2 +0,0 @@
-// lib/config.js
-export const API_BASE_URL = "http://localhost:5000";
